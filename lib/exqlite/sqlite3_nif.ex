6a15f01fa66297cab23a05c804f00cc737fed01a
kabei <kambei.sapote@gmail.com>
2023-08-13 19:54:17 -0400

    bind_step_release

--- lib/exqlite/sqlite3_nif.ex
+++ lib/exqlite/sqlite3_nif.ex
@@ -39,13 +39,17 @@ defmodule Exqlite.Sqlite3NIF do
   @spec step(db(), statement()) :: :done | :busy | {:row, row()} | {:error, reason()}
   def step(_conn, _statement), do: :erlang.nif_error(:not_loaded)
 
-  @spec bind_and_step(db(), statement(), list()) ::
+  @spec bind_step(db(), statement(), list()) ::
           :done | :busy | {:row, row()} | {:error, reason()}
-  def bind_and_step(_conn, _statement, _args), do: :erlang.nif_error(:not_loaded)
+  def bind_step(_conn, _statement, _args), do: :erlang.nif_error(:not_loaded)
 
   @spec bind_step_changes(db(), statement(), list()) ::
           integer() | :busy | {:row, row()} | {:error, reason()}
   def bind_step_changes(_conn, _statement, _args), do: :erlang.nif_error(:not_loaded)
+
+  @spec bind_step_release(db(), statement(), list()) ::
+          :done | :busy | {:row, row()} | {:error, reason()}
+  def bind_step_release(_conn, _statement, _args), do: :erlang.nif_error(:not_loaded)
 
   @spec multi_step(db(), statement(), integer()) ::
           :busy | {:rows, [row()]} | {:done, [row()]} | {:error, reason()}
